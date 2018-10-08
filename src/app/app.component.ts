c09e40a55bf6cc94677ce1ae941a79bf99702299
zuba5tik <thezuba5tik@gmail.com>
2018-10-08 20:41:47 +0300

    add two buttons that change second input type between text and dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
-// export interface Type {
-//   key: string;
+// export interface FormField {
 //   label: string;
+//   type: string;
+// }
+// export interface InputType {
+//   type: string;
+// }
+// export const inputTypes: InputType[] = [
+//   {type: 'text'}, {type: 'select'}
+// ];
+// export function getFormField(formField: FormField = null): FormGroup {
+//   return new FormGroup({
+//     label: new FormControl(formField ? formField.label : 'Label'),
+//     type: new FormControl(formField ? formField.type : inputTypes[0].type),
+//   });
 // }
 
 @Component({
@@ -16,13 +28,33 @@ export class AppComponent {
   animalForm: FormGroup;
   constructor(private fb: FormBuilder) {
     this.animalForm = this.fb.group({
-      name: [''],
-      type: [''],
+      animalName: [],
+      animalType: [],
+      // animalType: this.inputType === 'select' ? this.fb.group({
+      //   typeKey: '',
+      //   typeLabel: '',
+      // }) : '',
     });
   }
+
+  inputType = 'text';
 
   types = [
     {'key': 'dog-0', 'label': 'Dog'},
     {'key': 'cat-1', 'label': 'Cat'}
   ];
+
+  // GetControl(form: FormGroup, field: string) {
+  //   // tslint:disable-next-line:whitespace
+  //   const fieldType = document.querySelector('input[name="'+field+'"]');
+  //   if (fieldType.getAttribute('type') === 'text') {
+  //     console.log('nice');
+  //   }
+  //   return form.get(field);
+  // }
+
+  // fieldType = [
+  //   {type: 'text', placeholder: 'Animal Type text'},
+  //   {type: 'select', placeholder: 'Animal Type select'}
+  // ];
 }
