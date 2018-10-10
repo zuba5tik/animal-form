6866f6c7cda51e48a19e8d43a9cbb0f8b084d0d5
zuba5tik <thezuba5tik@gmail.com>
2018-10-10 15:32:25 +0300

    toggling between input and dropdown for second field with one button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
-// export interface FormField {
-//   label: string;
-//   type: string;
-// }
-// export interface InputType {
-//   type: string;
-// }
-// export const inputTypes: InputType[] = [
-//   {type: 'text'}, {type: 'select'}
-// ];
-// export function getFormField(formField: FormField = null): FormGroup {
-//   return new FormGroup({
-//     label: new FormControl(formField ? formField.label : 'Label'),
-//     type: new FormControl(formField ? formField.type : inputTypes[0].type),
-//   });
-// }
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -25,39 +8,34 @@ import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 })
 export class AppComponent {
   title = 'animal-app';
+  inputType = 'select';
   animalForm: FormGroup;
-  animalType = new FormGroup({
+  animalType = this.inputType === 'select' ? new FormGroup({
     typeLabel: new FormControl(),
     typeKey: new FormControl(),
-  });
+  }) : new FormControl();
   constructor(private fb: FormBuilder) {
     this.animalForm = this.fb.group({
       animalName: [],
-      animalType: [],
-      // animalType: this.animalType = this.fb.group({
-      //   typeLabel: [],
-      //   typeKey: [],
-      // })
+      animalType: this.inputType === 'select' ? new FormGroup({
+        typeLabel: new FormControl(),
+        typeKey: new FormControl()}) : [],
     });
   }
 
   inputDropDown() {
-    // this.animalType.addControl('typeKey', new FormControl());
-    this.animalForm.reset();
-    this.animalForm.removeControl('animalType');
-    this.animalForm.registerControl('animalType', new FormGroup({typeLabel: new FormControl(), typeKey: new FormControl()}));
-    // this.animalType.addControl('typeLabel', new FormControl());
-    // this.animalType.addControl('typeKey', new FormControl());
-    this.inputType = 'select';
+    if (this.inputType === 'select') {
+        this.animalForm.reset();
+        this.animalForm.removeControl('animalType');
+        this.animalForm.addControl('animalType', new FormControl());
+        this.inputType = 'text';
+    } else if (this.inputType === 'text') {
+        this.animalForm.reset();
+        this.animalForm.removeControl('animalType');
+        this.animalForm.registerControl('animalType', new FormGroup({typeLabel: new FormControl(), typeKey: new FormControl()}));
+        this.inputType = 'select';
+    }
   }
-
-  inputText() {
-    this.animalForm.reset();
-    this.animalForm.removeControl('animalType');
-    this.animalForm.addControl('animalType', new FormControl());
-    this.inputType = 'text';
-  }
-
   // getField() {
   //   this.types = [
   //     {
@@ -69,25 +47,11 @@ export class AppComponent {
   // }
 
   // tslint:disable-next-line:member-ordering
-  inputType = 'text';
+  // inputType = 'select';
 
   // tslint:disable-next-line:member-ordering
   types = [
     {'key': 'dog-0', 'label': 'Dog'},
     {'key': 'cat-1', 'label': 'Cat'}
   ];
-
-  // GetControl(form: FormGroup, field: string) {
-  //   // tslint:disable-next-line:whitespace
-  //   const fieldType = document.querySelector('input[name="'+field+'"]');
-  //   if (fieldType.getAttribute('type') === 'text') {
-  //     console.log('nice');
-  //   }
-  //   return form.get(field);
-  // }
-
-  // fieldType = [
-  //   {type: 'text', placeholder: 'Animal Type text'},
-  //   {type: 'select', placeholder: 'Animal Type select'}
-  // ];
 }
